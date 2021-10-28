3172d635a5f8a73a4d4a761e8f00fd0b96adacce
SYED MD SAIFI HASSAN <syedsaifihassan@gmail.com>
2021-10-29 03:49:18 +0530

    get address of cells

--- Browser/Module_4_Excel_Clone/script.js
+++ Browser/Module_4_Excel_Clone/script.js
@@ -1,22 +1,7 @@
-{/* <div class="cell">
-        C
-    </div> */}
-
-// create cells
-// let div = document.createElement("div");
-// div.setAttribute("class", "cell");
-// div.textContent = "A";
-// let topRow = document
-//     .querySelector(".top_row");
-// topRow.appendChild(div)
-// A-> Z
-// Ascii -> A
-// for loop for prinitng alphabets
-
 let rows = 100;
 let cols = 26;
 
-// top rows
+// creating top rows cells
 let topRow = document.querySelector(".top_row");
 for (let i = 0; i < cols; i++) {
     let div = document.createElement("div");
@@ -25,7 +10,7 @@ for (let i = 0; i < cols; i++) {
     topRow.appendChild(div)
 }
 
-// left column
+// creating left column cells
 let leftCol = document.querySelector(".left_col");
 for (let i = 1; i <= rows; i++) {
     let div = document.createElement("div");
@@ -43,9 +28,58 @@ for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
         let cell = document.createElement("div");
         cell.setAttribute("class", "cell");
+        cell.setAttribute("rId", i);
+        cell.setAttribute("cId", j);
         // cell.textContent = i+","+j;
         cell.contentEditable = true;
         row.appendChild(cell);
     }
     grid.appendChild(row);
+}
+
+// if i click any of the cells
+let allGridCells = document.querySelectorAll(".grid .cell");
+let addressInput = document.querySelector(".address_input");
+for (let i = 0; i < allGridCells.length; i++) {
+    allGridCells[i].addEventListener("click", function (e) {
+
+        let prevAddress = addressInput.value;
+        if (prevAddress != "") {
+            let ridcidObj = getRidCidFromAddress(prevAddress);
+            // alert(ridcidObj.rid +""+ ridcidObj.cid);
+            // prev -> remove -> border
+            let prevCell = document.querySelector(`.grid .cell[rId='${ridcidObj.rid}'][cId='${ridcidObj.cid}']`);
+            prevCell.style.border ="0.5px solid gray";
+            // prevCell.style.borderRight="none";
+            // prevCell.style.borderTop="none";
+        }
+        // alert("cell is clicked");
+        // then i will get the address of that particular cell
+        let rid = allGridCells[i].getAttribute("rId");
+        let cid = allGridCells[i].getAttribute("cId");
+        // get -> always in
+        rid = Number(rid);
+        cid = Number(cid);
+        // alert(rid+1+" "+String.fromCharCode(65+cid));
+        addressInput.value = String.fromCharCode(65+cid)+(rid+1);
+        // cell styling bhi change
+        let cCell = allGridCells[i];
+        cCell.style.border = "2px solid #1B9CFC";
+    })
+}
+
+// get first element (default)
+let firstCell = document.querySelector(".grid .cell[rId='0'][cId='0']");
+firstCell.click();
+firstCell.focus();
+
+function getRidCidFromAddress(address) {
+    // A-Z, 1-100
+    // B
+    let AsciiValue = address.charCodeAt(0);
+    let cid = AsciiValue - 65;
+    let rid = Number(address.substring(1)) - 1;
+    return {
+        rid: rid, cid: cid
+    }
 }
